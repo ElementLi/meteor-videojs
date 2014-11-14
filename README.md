d804ea01310969ec84846977a9d695cd82944aff
yonglisc <yonglisc@gmail.com>
2014-11-14 00:26:50 -0800

    Update to 4.10.2

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 meteor-videojs
 ==============
 
-***Videojs 4.9.1 for Meteor
+***Videojs 4.10.2 for Meteor
 
 A package for Meteor users to use videojs easily.
--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name:"yong:meteor-videojs",
-  summary: "Video.js 4.9.1",
-  version: "1.0.1",
+  summary: "Video.js 4.10.2",
+  version: "1.0.2",
   git: " https://github.com/feelsys/meteor-videojs.git "
 });
 
